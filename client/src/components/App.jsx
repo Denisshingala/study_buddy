49a99b3908391f6223334251cac51aa8f7e24cd8
Denis-Shingala <denisshingala@gmail.com>
2022-03-01 16:23:59 +0530

    Done

---
+++ client/src/components/App.jsx
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import NotFound from './404'
+// import Dashboard from './dashboard/Dashboard'
+// import Course from './courses/Course'
+// import Notification from './notification/notification'
+import Signup from './Signup'
+import Login from './Login'
+import Index from './Index'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Home';
+import Footer from './Footer';
+
+const App = () => {
+    return (<>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Index />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+                <Route exact path="/home/*" element={<Home />} />
+                <Route element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+        <Footer />
+    </>
+    )
+}
+
+export default App
---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+import axios from 'axios'
+import './dashboard.css'
+import DataFetching from './DataFetching'
+import {  useNavigate } from "react-router-dom"
+
+const Dashboard = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const callBack = async () => {
+            await axios.get("/dashboard")
+                .then((result) => {
+                    if (!result.data)
+                        navigate('/')
+                })
+        }
+        callBack();
+    }, [navigate]);
+
+    return (
+        <>
+            <div className="dashboard-view">
+                <DataFetching />
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
